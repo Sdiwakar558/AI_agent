eb5d7e2e8ab9fd7486413b505660039dbecb2e58
nitesh kumar <knitesh706@gmail.com>
2025-05-25 14:27:52 +0530

    ignore .env file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.data_feeding_llm import rag_chain
-
+from src.embed_new_data import read_n_embed_data
+import os
 def fetch_data_rag(user_input):
+    if os.path.exists('./chroma_db'):
+        pass
+    else:
+        read_n_embed_data()
     response = rag_chain().invoke({'question':user_input})
     return response
 
